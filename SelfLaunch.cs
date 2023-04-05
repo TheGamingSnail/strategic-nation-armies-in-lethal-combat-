e1ea8f5097c0feafbd537b761064bd5b9e4e7b92
Snail <thisistheemailthatiusedlmao@gmail.com>
2023-04-05 19:30:05 +0300

    I WANNA FUCKING DIE

--- SelfLaunch.cs
+++ SelfLaunch.cs
@@ -8,15 +8,27 @@ public class SelfLaunch : MonoBehaviour
     // Start is called before the first frame update
     public Transform target;
     [SerializeField] private string findName = "spear";
+
+    [SerializeField] private float aimDis = 10;
     [SerializeField] private float angle;
 
     private void Start()
     {
+        float velocity = 0;
         NavMeshAgent navMA = target.GetComponent<NavMeshAgent>();
-        float distance = Vector3.Distance(transform.position, target.position + target.forward * navMA.speed);
-
-        float velocity = distance / (Mathf.Sin(2 * angle * Mathf.Deg2Rad) / Physics.gravity.magnitude);
-
+        float distance = Vector3.Distance(transform.position, target.position);
+        Debug.Log(distance + " " + aimDis);
+        if (distance >= aimDis)
+        {
+            distance = Vector3.Distance(transform.position, target.position + target.forward * navMA.speed);
+        }
+        else
+        {
+            distance =  Vector3.Distance(transform.position, target.position);
+        }
+        Debug.Log(distance + " " + Vector3.Distance(transform.position, target.position) + " " + (Vector3.Distance(transform.position, target.position) >= aimDis));
+        
+        velocity = distance / (Mathf.Sin(2 * angle * Mathf.Deg2Rad) / Physics.gravity.magnitude);
         float xVelocity = Mathf.Sqrt(velocity) * Mathf.Cos(angle * Mathf.Deg2Rad);
         float yVelocity = Mathf.Sqrt(velocity) * Mathf.Sin(angle * Mathf.Deg2Rad);
 
